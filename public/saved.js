acde8029a36bd24248126d02656aa0798a534035
Jennifer Birsan <jennibirsan@Cornels-MBP.attlocal.net>
2019-08-29 16:35:05 -0500

    fixed css

--- public/saved.js
+++ public/saved.js
@@ -1,11 +1,11 @@
 $.getJSON("/saved", function(data) {
     for (var i = 0; i < data.length; i++) {
   
-      $("#articles").append(
-        "<div class='col-sm-4' style='margin-bottom:60px;'><div class='card'><img class='image' src=" + data[i].img + "></img><div class='card-body'><a class='title-link' href='" + data[i].link + "'><h5>" + data[i].title + "</h5></a><hr><p class='card-text'>" + data[i].snippet + "</p><button data-id='" + data[i]._id + "' class='btn-note btn btn-outline-primary btn-sm' data-toggle='modal' data-target='#myModal' style='margin-right:10px;'>Note</button><button id='btn-save' data-id='" + data[i]._id + "' class='btn btn-outline-primary btn-sm'>Save Article</button></div></div></div>"
-      );
-    }
-    
+    $("#articles").append(
+      "<div class='col-sm-4' style='margin-bottom:60px;'><div class='card'><img class='image' src=" + data[i].img + "></img><div class='card-body'><a class='title-link' href='" + data[i].link + "'><h5>" + data[i].title + "</h5></a><hr><p class='card-text'>" + data[i].snippet + "</p><button data-id='" + data[i]._id + "' class='btn-note btn btn-outline-primary btn-sm' data-toggle='modal' data-target='#myModal' style='margin-right:10px;'>Note</button><button id='btn-save' data-id='" + data[i]._id + "' class='btn btn-outline-primary btn-sm'>Save Article</button></div></div></div>"
+    );
+  }
+  
     console.log(data);
   });
   
